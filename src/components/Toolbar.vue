659a158923ffb3f6564666ae12867517b1647afd
Aaron Woods <aaron.woods22@mail.dcu.ie>
2018-12-12 19:33:58 +0000

    Basic Toolbar added

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-toolbar dark app>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Project</span>
+        <span class="font-weight-light">-vuejs</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/AaronWoods"
+        target="_blank"
+      >
+        <span class="mr-2">Github</span>
+      </v-btn>
+    </v-toolbar>
+</template>
+
+<<script>
+    
+</script>
