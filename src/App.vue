538c64f47f2519b5edd24e6d8873822521618d99
Aaron Woods <aaron.woods22@mail.dcu.ie>
2018-12-31 17:21:13 +0000

    Home page developed, modifying template to get started
    Homepage available.
    Fixed v-parallax issues

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
       <app-toolbar @toggleDrawer="toggleDrawer"></app-toolbar>
       <app-side-drawer :drawer='drawer'></app-side-drawer>
       <app-footer></app-footer>
-      <v-content></v-content>
+      <v-content>
+        <app-home></app-home>
+      </v-content>
     </v-app>
   </div>
 </template>
@@ -14,13 +16,15 @@
 import AppToolbar from './components/AppToolbar'
 import AppSideDrawer from './components/AppSideDrawer'
 import AppFooter from './components/AppFooter'
+import AppHome from './components/AppHome'
 
 export default {
   name: 'App',
   components: {
     AppToolbar,
     AppSideDrawer,
-    AppFooter
+    AppFooter,
+    AppHome
   },
   data () {
     return {
