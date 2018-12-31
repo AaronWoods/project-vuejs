134387cbd7bf5f36cc05d040c1f1fe687cdd3b51
AaronWoods <aaron.woods22@mail.dcu.ie>
2018-12-31 13:32:24 +0000

    Core Components
    Converted app to use components for root components, eg. Footer, AppDrawer and Toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,39 @@
 <template>
   <div id="app">
     <v-app id="inspire" dark>
-      <v-navigation-drawer
-        clipped
-        fixed
-        v-model="drawer"
-        app
-      >
-        <v-list dense>
-          <v-list-tile @click="doThis">
-            <v-list-tile-action>
-              <v-icon>dashboard</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Dashboard</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-          <v-list-tile @click="doThat">
-            <v-list-tile-action>
-              <v-icon>settings</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Settings</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
-      <v-toolbar app fixed clipped-left>
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <v-toolbar-title>{{siteTitle}}</v-toolbar-title>
-        <v-spacer></v-spacer>
-      <v-btn
-        flat
-        :href="gitHubHook"
-        target="_blank"
-      >
-        <span class="mr-2">Github</span>
-      </v-btn>
-      </v-toolbar>
-      <v-content>
-        
-      </v-content>
-      <v-footer app fixed>
-        <span>&copy; 2017</span>
-      </v-footer>
+      <app-toolbar @toggleDrawer="toggleDrawer"></app-toolbar>
+      <app-side-drawer :drawer='drawer'></app-side-drawer>
+      <app-footer></app-footer>
+      <v-content></v-content>
     </v-app>
   </div>
 </template>
 
 <script>
 
-import Toolbar from './components/Toolbar'
+import AppToolbar from './components/AppToolbar'
+import AppSideDrawer from './components/AppSideDrawer'
+import AppFooter from './components/AppFooter'
+
 export default {
   name: 'App',
   components: {
-    Toolbar
+    AppToolbar,
+    AppSideDrawer,
+    AppFooter
   },
   data () {
     return {
-      drawer: false,
       gitHubHook: "https://github.com/AaronWoods",
-      siteTitle: "project-vuejs"
+      siteTitle: "project-vuejs",
+      drawer: true
     }
   },
   methods:{
-    doThis : function() {
-      console.log("this");
-    },
-    doThat : function(){
-      console.log("that");
+    toggleDrawer : function(){
+      this.drawer = !this.drawer
     }
   }
+
 }
 </script>
---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="toggleDrawer"></v-toolbar-side-icon>
+      <v-toolbar-title>{{siteTitle}}</v-toolbar-title>
+      <v-spacer></v-spacer>
+    <v-btn
+      flat
+      :href="gitHubHook"
+      target="_blank"
+    >
+      <span class="mr-2">{{link1}}</span>
+    </v-btn>
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+      name: 'Toolbar',
+      data () {
+          return {
+              gitHubHook: "https://github.com/AaronWoods",
+              siteTitle: "project-vuejs",
+              link1: "Github"
+          }
+      },
+      methods:{
+        toggleDrawer : function(){
+          this.$emit('toggleDrawer')
+        }
+      }
+  }
+</script>
